87f4ec3a1fbc6045e6825de8a665e52ca6c7b8dc
Jake <jakegu2013@gmail.com>
2026-02-09 22:28:03 -0500

    added main.py template

--- src/task_cli/main.py
+++ src/task_cli/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from task-tracker!")
+import typer
+from typing import Annotated
 
+app = typer.Typer()
+
+@app.command()
+def hello(name: Annotated[str, typer.Option()] = "penis"):
+    print(f"Hello {name}")
 
 if __name__ == "__main__":
-    main()
+    app()
